7e54ebc539f820901c51c46b61f395ae4d0b3931
BasilAhmedd <151678545+BasilAhmedd@users.noreply.github.com>
2024-05-26 11:35:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function fun(){
+    document.getElementById('four').style = "display : none"
+    document.getElementById('five').style = "display : block;position: absolute;left: 300px; top: 290px;"
+}
+
+function vv(){
+   var i = document.getElementById('bee1').style = "display : none";
+   i.src = "camel.png"
+    document.getElementById('ph').style = "display : block;"
+}
+function ff(){
+    var i = document.getElementById('ph').style = "display : none";
+    i.src = "bee.png"
+    document.getElementById('bee1').style = "display : block;"
+ }
